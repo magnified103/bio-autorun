cee39adb39b0861df512043e40c058657b6e326d
Dung Nguyen <29406816+magnified103@users.noreply.github.com>
2025-07-23 09:39:37 +0000

    feat: add fasta dataset

--- src/bio_autorun/datasets/treebase.py
+++ src/bio_autorun/datasets/treebase.py
@@ -8,10 +8,24 @@ class TreebaseDataset(Dataset):
         self.msa_list: list[MSA] = []
         for name in os.listdir(data_dir):
             if name.endswith(".phy"):
-                self.msa_list.append(MSA(name, os.path.abspath(os.path.join(data_dir, name))))
-    
+                self.msa_list.append(MSA(name[:-4], os.path.abspath(os.path.join(data_dir, name))))
+
     def __getitem__(self, index: int) -> MSA:
         return self.msa_list[index]
-    
+
+    def __len__(self) -> int:
+        return len(self.msa_list)
+
+
+class TreebaseFastaDataset(Dataset):
+    def __init__(self, data_dir: str):
+        self.msa_list: list[MSA] = []
+        for name in os.listdir(data_dir):
+            if name.endswith(".fasta"):
+                self.msa_list.append(MSA(name[:-6], os.path.abspath(os.path.join(data_dir, name))))
+
+    def __getitem__(self, index: int) -> MSA:
+        return self.msa_list[index]
+
     def __len__(self) -> int:
         return len(self.msa_list)
